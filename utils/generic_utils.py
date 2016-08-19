f7fd165490690b1ddde8fe62ab35f3f3505f0813
cubestone <kubamagist@o2.pl>
2016-08-20 00:21:57 +0200

    tried lagging and features importance for simple models

--- utils/generic_utils.py
+++ utils/generic_utils.py
@@ -1,10 +1,18 @@
 import cPickle as pickle
+from sklearn.externals import joblib
 
-def pickle_in(datapath):
-    with open(datapath,"rb") as f:
-        data = pickle.load(f)
-    return data
-
-def pickle_out(datapath,data):
-    with open(datapath,"wb") as f:
-        pickle.dump(data,f)
+def pickle_in(datapath,compresion_mode=0):
+    if compresion_mode ==0:
+        with open(datapath,"rb") as f:
+            data = pickle.load(f)
+        return data
+    else:
+        data = joblib.load(datapath)
+        return data
+        
+def pickle_out(datapath,data,compresion_mode = 0):
+    if compresion_mode ==0:
+        with open(datapath,"wb") as f:
+            pickle.dump(data,f)
+    else:
+        joblib.dump(data, datapath, compress=compresion_mode)
--- utils/preprocessing_utils.py
+++ utils/preprocessing_utils.py
@@ -1,42 +1,163 @@
 import numpy as np
+import pandas as pd
 
-def preprocess_vanila_random_forest(data,mode= "train"):
+def preprocess_vanila(data,mode= "train"):
+    data = data.sort_values(["datetime"])
+    
     if mode =="train":
         X = data.drop(["count","registered","casual","datetime"], axis=1)
         Y = data[["count"]].values.ravel()
     
         return (X,Y)
+    
     elif mode =="test":
+        
         X = data.drop(["datetime"], axis=1)
         Index = data[["datetime"]]
 
         return (X,Index)
+    
     else:
+        
         raise Exception("Wrong mode")
         
-def get_hour(datetime):
-    return
-def get_day_of_the_week(datetime):
-    return
-def get_month(datetime):
-    return
-def get_year(datetime):   
-    return
-        
-def preprocess_weekdays_random_forest(data,mode= "train"):
+def preprocess_weekdays(data,mode= "train"):
+    data = data.sort_values(["datetime"])
+    
     if mode =="train":
-        X = data.drop(["count","registered","casual"], axis=1)
+        
+        data["datetime"] =  pd.to_datetime(data["datetime"], 
+                                           format="%Y-%m-%d %H:%M:%S")
+        data["time_hour"] = data["datetime"].apply(lambda x: x.hour)
+        data["date_weekday"] = data["datetime"].apply(lambda x: x.weekday())
+        data["date_month"] = data["datetime"].apply(lambda x: x.month)
+        data["date_year"] = data["datetime"].apply(lambda x: x.year)
+        
+        X = data.drop(["count","registered","casual","datetime"], axis=1)
         Y = data[["count"]].values.ravel()
-        X["hour"] = X["datetime"]
-        X["day"] = X["datetime"]
-        X["month"] = X["datetime"]
-        X["year"] = X["datetime"]
         
         return (X,Y)
+    
     elif mode =="test":
+        
+        data["datetime"] =  pd.to_datetime(data["datetime"], 
+                                           format="%Y-%m-%d %H:%M:%S")
+        data["time_hour"] = data["datetime"].apply(lambda x: x.hour)
+        data["date_weekday"] = data["datetime"].apply(lambda x: x.weekday())
+        data["date_month"] = data["datetime"].apply(lambda x: x.month)
+        data["date_year"] = data["datetime"].apply(lambda x: x.year)
+        
         X = data.drop(["datetime"], axis=1)
         Index = data[["datetime"]]
 
         return (X,Index)
+    
     else:
+        
         raise Exception("Wrong mode")
+        
+def preprocess_weekdays_important(data,mode= "train"):
+    data = data.sort_values(["datetime"])
+    
+    if mode =="train":
+        
+        data["datetime"] =  pd.to_datetime(data["datetime"], 
+                                           format="%Y-%m-%d %H:%M:%S")
+        data["time_hour"] = data["datetime"].apply(lambda x: x.hour)
+        data["date_weekday"] = data["datetime"].apply(lambda x: x.weekday())
+        data["date_month"] = data["datetime"].apply(lambda x: x.month)
+        data["date_year"] = data["datetime"].apply(lambda x: x.year)
+        
+        X = data.drop(["count","registered","casual",
+                       "datetime","weather","season",
+                       "windspeed","holiday"], axis=1)
+        Y = data[["count"]].values.ravel()
+        
+        return (X,Y)
+    
+    elif mode =="test":
+        
+        data["datetime"] =  pd.to_datetime(data["datetime"], 
+                                           format="%Y-%m-%d %H:%M:%S")
+        data["time_hour"] = data["datetime"].apply(lambda x: x.hour)
+        data["date_weekday"] = data["datetime"].apply(lambda x: x.weekday())
+        data["date_month"] = data["datetime"].apply(lambda x: x.month)
+        data["date_year"] = data["datetime"].apply(lambda x: x.year)
+        
+        X = data.drop(["datetime","weather","season",
+                       "windspeed","holiday"], axis=1)
+        Index = data[["datetime"]]
+
+        return (X,Index)
+    
+    else:
+        
+        raise Exception("Wrong mode")
+        
+def preprocess_weekdays_important_strict(data,mode= "train"):
+    data = data.sort_values(["datetime"])
+    
+    if mode =="train":
+        
+        data["datetime"] =  pd.to_datetime(data["datetime"], 
+                                           format="%Y-%m-%d %H:%M:%S")
+        data["time_hour"] = data["datetime"].apply(lambda x: x.hour)
+        data["date_weekday"] = data["datetime"].apply(lambda x: x.weekday())
+        data["date_month"] = data["datetime"].apply(lambda x: x.month)
+        data["date_year"] = data["datetime"].apply(lambda x: x.year)
+        
+        X = data.drop(["count","registered","casual",
+                       "datetime","weather","season",
+                       "windspeed","holiday","humidity",
+                       "date_weekday","date_month"], axis=1)
+        Y = data[["count"]].values.ravel()
+        
+        return (X,Y)
+    
+    elif mode =="test":
+        
+        data["datetime"] =  pd.to_datetime(data["datetime"], 
+                                           format="%Y-%m-%d %H:%M:%S")
+        data["time_hour"] = data["datetime"].apply(lambda x: x.hour)
+        data["date_weekday"] = data["datetime"].apply(lambda x: x.weekday())
+        data["date_month"] = data["datetime"].apply(lambda x: x.month)
+        data["date_year"] = data["datetime"].apply(lambda x: x.year)
+        
+        X = data.drop(["datetime","weather","season",
+                       "windspeed","holiday","humidity",
+                       "date_weekday","date_month"], axis=1)
+        Index = data[["datetime"]]
+
+        return (X,Index)
+    
+    else:
+        
+        raise Exception("Wrong mode")
+        
+def generate_timelag_data(data,lag = 3):
+    final = data
+    lagging_set = data.drop(["time_hour","date_weekday",
+                             "date_month","date_year",
+                             "workingday","holiday","season"],axis = 1)
+    for i in range(1,lag,1):
+        x = lagging_set
+        x = x.shift(i)
+        x.rename(columns=lambda x: x+"_lag_%s"%i, inplace=True)
+        final = pd.concat([final,x], axis=1)
+    final = final.fillna(method="bfill")
+   
+    return final
+
+def generate_smart_timelag_data(data,lag = 3):
+    final = data
+    lagging_set = data.drop(["time_hour","date_weekday",
+                             "date_month","date_year",
+                             "workingday","holiday","season"],axis = 1)
+    for i in range(1,lag,1):
+        x = lagging_set
+        x = x.shift(i)
+        x.rename(columns=lambda x: x+"_lag_%s"%i, inplace=True)
+        final = pd.concat([final,x], axis=1)
+    final = final.fillna(method="bfill")
+   
+    return final
